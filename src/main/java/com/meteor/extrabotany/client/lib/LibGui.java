5402afafd83a32bdaf35e6a8dc9ceafa20cf29fb
ExtraMeteorP <1579205460@qq.com>
2019-01-24 17:28:51 +0800

    the work of satan

--- src/main/java/com/meteor/extrabotany/client/lib/LibGui.java
+++
@@ -1,7 +0,0 @@
-package com.meteor.extrabotany.client.lib;
-
-public class LibGui {
-	
-	public static final int MASTERHANDBAG = 0;
-
-}
--- src/main/java/com/meteor/extrabotany/common/item/equipment/bauble/stone/ItemPowerStone.java
+++
@@ -1,5 +0,0 @@
-package com.meteor.extrabotany.common.item.equipment.bauble.stone;
-
-public class ItemPowerStone {
-
-}
